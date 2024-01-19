7a3a747ea2d04c33d6332f98107f5a64b883645c
daodc <chidao1080@gmail.com>
2024-01-19 17:44:08 +0700

    Init Project

---
+++ EnglishLearningApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using EnglishLearningApp;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+
+var builderWeb = WebHost.CreateDefaultBuilder(args)
+           .UseUrls("http://localhost:4000") // Cổng mong muốn
+           .UseStartup<StartUp>();
+
+
+
+var webHost = builderWeb.Build();
+
+
+
+webHost.Run();
+
+
+
+/*var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddTransient<IStartup, StartUp>();
+
+
+
+
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+
+
+
+  *//*  app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();*//*
+}
+
+
+
+*/
+
+
+
+
+
+/*builderWeb.Build().Run();*/
+
+/*app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();*/
---
+++ EnglishLearningApp/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace EnglishLearningApp
+{
+
+    public class StartUp
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapRazorPages();
+
+                endpoint.MapGet("/test", async app =>
+                {
+                    await app.Response.WriteAsync("Hello");
+                });
+
+                
+            });
+           
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {   
+            services.AddControllers();
+
+            services.AddRazorPages();
+            // Thực hiện các tùy chỉnh và trả về IServiceProvider
+           /* return services.BuildServiceProvider();*/
+
+        }
+
+    }
+}
